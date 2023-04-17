20f834558e58adc0dcae59b7833ae64be8ee70f6
amit <amit256375@gmail.com>
2023-04-17 22:45:58 +0530

    change type of number

--- models/contact.js
+++ models/contact.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
+import { number } from 'yup';
 
 const ContactSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  mobileNumber: { type: String, required: true },
-  companyNumber: { type: String, required: true },
+  mobileNumber: { type: Number, required: true },
+  companyNumber: { type: Number, required: true },
   email: { type: String, required: true },
   companyLogo: { type: String, required: true },
   deck: { type: String, required: true },
--- pages/ContactForm.js
+++ pages/ContactForm.js
@@ -16,8 +16,8 @@ const ContactForm = () => {
   const validationSchema = Yup.object({
     firstName: Yup.string().required('First name is required'),
     lastName: Yup.string().required('Last name is required'),
-    mobileNumber: Yup.string().required('Mobile number is required'),
-    companyNumber: Yup.string().required('Company number is required'),
+    mobileNumber: Yup.number().required('Mobile number is required'),
+    companyNumber: Yup.number().required('Company number is required'),
     email: Yup.string().email('Invalid email address').required('Email is required'),
     companyLogo: Yup.string().required('Company logo is required'),
     deck: Yup.string().required('Deck is required'),
@@ -33,50 +33,110 @@ const ContactForm = () => {
       };
      
       return (
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-          {({ isSubmitting }) => (
-            <Form>
-              <div>
-                <label htmlFor="firstName">First Name:</label>
-                <Field type="text" id="firstName" name="firstName" />
-                <ErrorMessage name="firstName" component="div" className="error" />
-              </div>
-              <div>
-                <label htmlFor="lastName">Last Name:</label>
-                <Field type="text" id="lastName" name="lastName" />
-                <ErrorMessage name="lastName" component="div" className="error" />
-              </div>
-              <div>
-                <label htmlFor="mobileNumber">Mobile Number:</label>
-                <Field type="text" id="mobileNumber" name="mobileNumber" />
-                <ErrorMessage name="mobileNumber" component="div" className="error" />
-              </div>
-              <div>
-                <label htmlFor="companyNumber">Company Number:</label>
-                <Field type="text" id="companyNumber" name="companyNumber" />
-                <ErrorMessage name="companyNumber" component="div" className="error" />
-              </div>
-              <div>
-                <label htmlFor="email">Email:</label>
-                <Field type="email" id="email" name="email" />
-                <ErrorMessage name="email" component="div" className="error" />
-              </div>
-              <div>
-                <label htmlFor="companyLogo">Company Logo:</label>
-                <Field type="text" id="companyLogo" name="companyLogo" />
-                <ErrorMessage name="companyLogo" component="div" className="error" />
-              </div>
-              <div>
-                <label htmlFor="deck">Deck:</label>
-                <Field type="text" id="deck" name="deck" />
+        <div className="flex justify-center">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form className="w-full max-w-md">
+                <div className="mb-4 mt-4">
+                  <label htmlFor="firstName" className="block text-gray-700 font-bold mb-2">
+                    First Name:
+                  </label>
+                  <Field
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage name="firstName" component="div" className="text-red-500" />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="lastName" className="block text-gray-700 font-bold mb-2">
+                    Last Name:
+                  </label>
+                  <Field
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage name="lastName" component="div" className="text-red-500" />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="mobileNumber" className="block text-gray-700 font-bold mb-2">
+                    Mobile Number:
+                  </label>
+                  <Field
+                    type="text"
+                    id="mobileNumber"
+                    name="mobileNumber"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage name="mobileNumber" component="div" className="text-red-500" />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="companyNumber" className="block text-gray-700 font-bold mb-2">
+                    Company Number:
+                  </label>
+                  <Field
+                    type="text"
+                    id="companyNumber"
+                    name="companyNumber"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage name="companyNumber" component="div" className="text-red-500" />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+                    Email:
+                  </label>
+                  <Field
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage name="email" component="div" className="text-red-500" />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="companyLogo" className="block text-gray-700 font-bold mb-2">
+                    Company Logo:
+                  </label>
+                  <Field
+                    type="text"
+                    id="companyLogo"
+                    name="companyLogo"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage name="companyLogo" component="div" className="text-red-500" />
+                </div>
+      
+              <div className="mb-4">
+                <label htmlFor="deck" lassName="block text-gray-700 font-bold mb-2">Deck:</label>
+                <Field 
+                type="text" 
+                id="deck" 
+                name="deck" 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+
+                
+                />
                 <ErrorMessage name="deck" component="div" className="error" />
               </div>
-              <button type="submit" disabled={isSubmitting}>
+              <div className="mb-4">
+
+              <button   className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+ type="submit" disabled={isSubmitting}>
                 Submit
               </button>
+              </div>
             </Form>
           )}
         </Formik>
+        </div>
       );
     };
 
