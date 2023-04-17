3dc8b82e8473218e25a85ba76a7aed3c8e83174e
amit <amit256375@gmail.com>
2023-04-17 23:32:09 +0530

    adding get method in api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,29 +1,40 @@
 import { connectToDatabase } from '../../lib/mongoose';
-import Contact from '../../models/contact'
+import Contact from '../../models/contact';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
+  if (req.method === 'GET') {
+    try {
+      await connectToDatabase();
+      const contacts = await Contact.find();
+      return res.status(200).json({ contacts });
+    } catch (error) {
+      return res.status(500).json({ message: 'Unable to retrieve contacts', error });
+    }
   }
 
-  await connectToDatabase();
+  if (req.method === 'POST') {
+    try {
+      await connectToDatabase();
 
-  const { firstName, lastName, mobileNumber, companyNumber, email, companyLogo, deck } = req.body;
+      const { firstName, lastName, mobileNumber, companyNumber, email, companyLogo, deck } = req.body;
 
-  const contact = new Contact({
-    firstName,
-    lastName,
-    mobileNumber,
-    companyNumber,
-    email,
-    companyLogo,
-    deck,
-  });
+      const contact = new Contact({
+        firstName,
+        lastName,
+        mobileNumber,
+        companyNumber,
+        email,
+        companyLogo,
+        deck,
+      });
 
-  try {
-    await contact.save();
-    return res.status(201).json({ message: 'Contact created successfully' });
-  } catch (error) {
-    return res.status(500).json({ message: 'Unable to create contact', error });
+      await contact.save();
+
+      return res.status(201).json({ message: 'Contact created successfully' });
+    } catch (error) {
+      return res.status(500).json({ message: 'Unable to create contact', error });
+    }
   }
+
+  return res.status(405).json({ message: 'Method not allowed' });
 }
