354aafe31cf8e8193777ef0b3b868f6e5da6c505
amit <amit256375@gmail.com>
2023-04-25 07:33:32 +0530

    changes in first

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import First from "@/components/First";
 import ContactForm from "./ContactForm";
 
 
 export default function Home() {
   return (
    <div>
-    <ContactForm/>
+    {/* <ContactForm/> */}
+    <First/>
    </div>
   )
 }
