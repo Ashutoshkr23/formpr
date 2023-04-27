83d4635a1482a45a61a618ce2d77b38ce574bbff
amit <amit256375@gmail.com>
2023-04-27 13:47:53 +0530

    add useeffect

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
 import First from "@/components/First";
-import { useSession, signIn, getSession,signOut } from "next-auth/react"
+import { useSession, signIn, getSession, signOut } from "next-auth/react"
 import axios from "axios";
 import { useEffect } from "react";
 import React from "react";
 
+export default function Home() {
+  const { data: session } = useSession()
 
-useEffect(() => {
-  async function getUserProfile(){
-    const { data: session } = useSession()
-    console.log(session)
-    const response = await axios.post('/api/userprofile', {email:session.user.email});
-  
-  console.log(response)
-  
-  }
-    getUserProfile()    
-     }, []);
+  useEffect(() => {
+    async function getUserProfile() {
+      const response = await axios.post('/api/userprofile', { email: session.user.email });
+      console.log(response)
+    }
+    if (session) {
+      getUserProfile()
+    }
+  }, [session]);
 
-export default  function Home () {
- 
   // if (session) {
   //   return (
   //     <>
@@ -27,31 +25,30 @@ export default  function Home () {
   //     </>
   //   )
   // }
+
   return (
-
-   <div>
-    <First/>
-   </div>
+    <div>
+      <First />
+    </div>
   )
-  
+}
 
-  }
-  export async function getServerSideProps({ req }) {
-    const session = await getSession({ req });
-    console.log(session, "session")
-    if (!session) {
-      return {
-        redirect: {
-          destination: '/login',
-          permanent: false,
-        },
-      };
-    }
-  
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+  console.log(session, "session")
+  if (!session) {
     return {
-      props: {
-        session,
-        
+      redirect: {
+        destination: '/login',
+        permanent: false,
       },
     };
   }
+
+  return {
+    props: {
+      session,
+
+    },
+  };
+}
