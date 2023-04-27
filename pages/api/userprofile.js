d4a1532b6485958883c5d179fe466dfe0d8122d7
amit <amit256375@gmail.com>
2023-04-27 18:55:58 +0530

    new

--- pages/api/userprofile.js
+++ pages/api/userprofile.js
@@ -2,8 +2,9 @@ import { connectToDatabase } from '../../lib/mongoose';
 import Userprofile from '@/models/userprofile';
 import { v4 as uuidv4 } from 'uuid';
 
-
 export default async function handler(req, res) {
+ 
+
   if (req.method === 'POST') {
     try {
       await connectToDatabase();
@@ -11,27 +12,22 @@ export default async function handler(req, res) {
       const { email } = req.body;
       const userprofile = await Userprofile.findOne({ email });
       if (userprofile) {
-        return res.status(200).json({ userprofile:userprofile});
+        return res.status(200).json({ message: 'User profile already exists', userprofile: userprofile });
+      } else {
+        const uuid = uuidv4(); // generate UUID
+        const newUserprofile = new Userprofile({
+          email,
+          uuid
+        });
+        await newUserprofile.save();
+        return res.status(201).json({ message: 'User profile created successfully', userprofile: newUserprofile });
       }
-      else
-      {
-      const uuid = uuidv4(); // generate UUID
-      const newUserprofile = new Userprofile({
-        email,uuid
-      });
-      await newUserprofile.save();
-      return res.status(201).json({ message: 'User profile created successfully',userprofile:newUserprofile });
-
-      }
-  
-     
-
-      
     } catch (error) {
-      return res.status(500).json({ message: 'problem in either fetching or creating user profile', error });
-
+      return res.status(500).json({ message: 'Problem in creating user profile', error });
     }
   }
+
+  return res.status(400).json({ message: 'Invalid request method' });
 }
 
   
