311a14d24460bbb2be39d8672ec0904ad1f9aab4
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-05-23 15:14:09 +0530

    Hero Section responsive

--- components/landing/Herof.js
+++ components/landing/Herof.js
@@ -8,8 +8,8 @@ const Herof = () => {
         <div className='flex  lg:flex-row justify-between   flex-col-reverse pt-24'>
 
             <div className="flex flex-col mt-48 ">
-                <h1 className='sm:text-[60px] bg-[#66D3E1] anim-typewriter lg:anim-typewriterlg text-[40px] font-bold  '>
-                    <span>Enter The </span> <span className="text-transparent bg-gradient-to-br from-[#66D3E1] to-[#96FFAD] bg-clip-text">
+                <h1 className='sm:text-[60px] mr-auto text-left anim-typewriter  text-[40px] font-bold '>
+                    Enter The <span className="text-transparent bg-gradient-to-br from-[#66D3E1] to-[#96FFAD] bg-clip-text">
                         Loop.
                     </span>
                 </h1>
@@ -23,27 +23,51 @@ const Herof = () => {
                 </motion.h6>
 
             </div>
-            <div className='ml-0 mx-auto lg:ml-auto lg:mx-0 pr-20'>
-                <div className=' relative'>
+            <div className=' hidden md:block  mx-auto lg:ml-auto lg:mx-0 lg:pr-20'>
+                <div className=' relative '>
                     <Image src={'/assets/images/landing/Mobile.png'}
                         width={258}
                         height={644}
                     />
                     <motion.div
-                        initial={{ x: -1000 }} // Initial position outside the left side of the screen
-                        animate={{ x: 0 }} // Animate to the original position
-                        transition={{ duration: 2 }} // Animation duration
+                        initial={{ x: -1000 }}
+                        animate={{ x: 0 }} 
+                        transition={{ duration: 2 }}
                         className='absolute top-[72px] -left-[175px]'
                     >
                         <Image src={'/assets/images/cardbl.svg'} width={244} height={177} />
                     </motion.div>
                     <motion.div
-                        initial={{ x: 1000 }} // Initial position outside the right side of the screen
-                        animate={{ x: 0 }} // Animate to the original position
-                        transition={{ duration: 2 }} // Animation duration
-                        className='absolute top-[180px] -z-10 -right-[127px]' 
+                        initial={{ x: 1000 }} 
+                        animate={{ x: 0 }} 
+                        transition={{ duration: 2 }} 
+                        className='absolute -z-10 top-[180px]  -right-[127px]' 
                     >
                         <Image src={'/assets/images/cardpi.svg'} width={221} height={174} /> 
+                    </motion.div>
+                </div>
+            </div>
+            <div className='md:hidden mx-auto '>
+                <div className=' relative'>
+                    <Image src={'/assets/images/landing/Mobile.png'}
+                        width={168}
+                        height={425}
+                    />
+                    <motion.div
+                        initial={{ x: -1000 }}
+                        animate={{ x: 0 }}
+                        transition={{ duration: 2 }}
+                        className='absolute top-[47px] -left-[115px] '
+                    >
+                        <Image src={'/assets/images/cardbl.svg'} width={162} height={117} />
+                    </motion.div>
+                    <motion.div
+                        initial={{ x: 1000 }}
+                        animate={{ x: 0 }}
+                        transition={{ duration: 2 }}
+                        className='absolute top-[118px]  -right-[87px] -z-10 '
+                    >
+                        <Image src={'/assets/images/cardpi.svg'} width={146} height={116} />
                     </motion.div>
                 </div>
             </div>
