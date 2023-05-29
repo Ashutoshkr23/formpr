80c7d524c139ad3271268180653ab20a5ec3c526
DurgeshJhalwa2805 <durgeshjhalwa2805@gmail.com>
2023-05-29 16:31:48 +0530

    .js to .jsx changes

--- models/card.js
+++ models/card.js
@@ -6,31 +6,30 @@ const CardSchema = new mongoose.Schema({
     // c uuid is card Id
     cuuid: { type: String, default: uuidv4, unique: true },
     puuid: { type: String, required: true },
-    // theme id will be coming from theme.js uuid
-    themeId: { type: String },
-    // card type will be id which will be coming from cardType.js object id
-    cardType: { type: mongoose.Schema.Types.ObjectId, required: true },
+    cardType: { type: mongoose.Schema.Types.ObjectId, required: true }, // card type will be id which will be coming from cardType.js object id
     cardAmount: { type: Number },
-    firstName: { type: String },
-    lastName: { type: String },
-    bio: { type: String },
+
+    fullName: { type: String },
+    cardName: { type: String },
+    mobileNumber: { type: String },
     companyName: { type: String },
     companyNumber: { type: String },
     companyLogo: { type: String },
-    mobileNumber: { type: String },
+    email: { type: String },
+    designation: { type: String },
+    bio: { type: String },
     websiteUrl: { type: String },
     // aws url will be stored in pfp
     profilePicture: { type: String },
-    email: { type: String },
-    designation: { type: String },
     instagramUrl: { type: String },
     whatsappUrl: { type: String },
     linkedinUrl: { type: String },
     facebookUrl: { type: String },
     contactUrl: { type: String },
     deckUrl: { type: String },
-    name: { type: String },
     avatar: { type: String },
+    themeId: { type: String },  // theme id will be coming from theme.js uuid
+
 
 });
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { LandingNav, Testimonial, Products, Story, Android, Hero, FAQ, Benefits} from '@/components'
+import { LandingNav, Testimonial, Products, Story, Android, Hero, FAQ, Benefits } from '@/components'
 
 const Landing = () => {
   return (
     <div className='bg-["#333333"] max-w-[1440px]  mx-auto px-4 sm:px-8 md:px-16 xl:px-20' >
       <LandingNav />
-      <Hero/>
-      <Benefits/>
+      <Hero />
+      <Benefits />
       <Products />
       <Story />
       <Testimonial />
       <Android />
-      <FAQ/>
+      <FAQ />
     </div>
   )
 }
