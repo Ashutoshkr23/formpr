2108efd014fe9c93de8a67880590494a6c05fe99
amit <amit256375@gmail.com>
2023-05-29 15:59:08 +0530

    phone motion

--- components/landing/Hero.jsx
+++ components/landing/Hero.jsx
@@ -25,52 +25,82 @@ const Hero = () => {
             </div>
             <div className=' hidden md:block  mx-auto lg:ml-auto lg:mx-0 lg:pr-20'>
                 <div className=' relative '>
-                    <Image src={'/assets/images/landing/mob.png'}
-                        width={258}
-                        height={644}
+                <motion.div
+                        initial={{ y: -250 }}
+                        animate={{ y: 0 }} 
+                        transition={{ duration: 2 }}
+                    >
+                    <Image src={'/assets/images/landing/phone1.png'}
+                        width={233}
+                        height={494}
                         quality={100}
                         alt='mobile'
                     />
-                    <motion.div
-                        initial={{ x: -250 }}
-                        animate={{ x: 0 }} 
-                        transition={{ duration: 2 }}
+                    </motion.div>
+                    <motion.div 
+                    initial={{ y: 200}}
+                    animate={{ y: 0 }} 
+                    transition={{ duration: 2 }}
+                    className='absolute top-[484px]'>
+                    <Image src={'/assets/images/landing/phone2.png'}
+                        width={233}
+                        height={154}
+                        quality={100}
+                        alt='mobile'
+                    /></motion.div>
+                    <div
+                     
                         className='absolute top-[72px] -left-[175px]'
                     >
                         <Image src={'/assets/images/cardbl.svg'} width={244} height={177} alt='card bl' />
-                    </motion.div>
-                    <motion.div
-                        initial={{ x: 1000 }} 
-                        animate={{ x: 0 }} 
-                        transition={{ duration: 2 }} 
+                    </div>
+                    <div
+                        
                         className='absolute -z-10 top-[180px]  -right-[127px]' 
                     >
                         <Image src={'/assets/images/cardpi.svg'} width={221} height={174} alt='card pi' /> 
-                    </motion.div>
+                    </div>
+
+                    
                 </div>
             </div>
             <div className='md:hidden mx-auto min-w-[375px] overflow-hidden '>
                 <div className=' relative mx-auto w-[200px] pl-8 '>
-                    <Image src={'/assets/images/landing/mob.png'}
+                <motion.div
+                        initial={{ y: -250 }}
+                        animate={{ y: 0 }} 
+                        transition={{ duration: 2 }}
+                    >
+                    <Image src={'/assets/images/landing/phone1.png'}
+                        width={168}
+                        height={332}
+                        alt='mobile'
+                    />
+                    </motion.div>
+                    <div
+                   
+                        className='absolute top-[47px] -left-[96px] '
+                    >
+                        <Image src={'/assets/images/cardbl.svg'} width={162} height={117} alt='card bl' />
+                    </div>
+                    <div
+                        
+                        className='absolute top-[118px]  -right-[87px] -z-10 '
+                    >
+                        <Image src={'/assets/images/cardpi.svg'} width={146} height={116} alt='card pi' />
+                    </div>
+                    <motion.div
+                        initial={{ y: 100 }}
+                        animate={{ y: 0 }} 
+                        transition={{ duration: 2 }}
+                        className='mt-[-5px] z-40 '
+
+                    >
+                    <Image src={'/assets/images/landing/phone2.png'}
                         width={168}
                         height={425}
                         alt='mobile'
                     />
-                    <motion.div
-                        initial={{ x: -400 }}
-                        animate={{ x: 0 }}
-                        transition={{ duration: 2 }}
-                        className='absolute top-[47px] -left-[96px] '
-                    >
-                        <Image src={'/assets/images/cardbl.svg'} width={162} height={117} alt='card bl' />
-                    </motion.div>
-                    <motion.div
-                        initial={{ x: 1000 }}
-                        animate={{ x: 0 }}
-                        transition={{ duration: 2 }}
-                        className='absolute top-[118px]  -right-[87px] -z-10 '
-                    >
-                        <Image src={'/assets/images/cardpi.svg'} width={146} height={116} alt='card pi' />
                     </motion.div>
                 </div>
             </div>
