0772a0827883a30f259ac60cb3687fff773bbba9
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-05-31 13:06:52 +0530

    add new swiper for pdoducts

--- components/landing/Products.jsx
+++ components/landing/Products.jsx
@@ -1,92 +1,82 @@
-import Image from 'next/image'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 import ProductComp from './ProductComp';
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+function Products() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const numDivs = 3;
+    const [numStep, setNumStep] = useState(3);
 
-const Products = () => {
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 768) {
+                setNumStep(numDivs - 0);
+            } else if (window.innerWidth < 1024) {
+                setNumStep(numDivs - 1);
+            } else {
+                setNumStep(numDivs - 2);
+            }
+        };
+
+        console.log(numStep)
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % numStep);
+    };
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + numStep) % numStep);
+    };
+
     return (
-        <div className='pt-[194px] '>
-            <div className='flex flex-col'>
-                <div className='flex flex-col justify-center items-center '>
-                    <div>
-                        <h6>Products</h6>
-                    </div>
-                        <h2 >Step into the Loop</h2>
-                </div>
-                <div className='hidden xl:block'>
-                    <div className='flex  justify-center pt-[92px]'>
-                        <ProductComp
-                            text={"Loop Lite"}
-                            img={"/assets/images/landing/LiteCard.png"}
-                            content={"Your brand's front-row showcase, in an affordable package."}
-                        />
-                        <ProductComp
-                            text={"Loop Elevate "}
-                            img={"/assets/images/landing/ElevateCard.png"}
-                            content={"Where your brand captivates, in seamless sophistication."}
-
-                        />
-                        <ProductComp
-                            text={"Loop Supreme"}
-                            img={"/assets/images/landing/SupremeCard.png"}
-                            content={"Where your brand commands attention, with unrivalled prestige."}
-
-                        />
-                    </div>
-                </div>
-                <div className='hidden md:block xl:hidden max-w-[800px] px-4 mx-auto'>
-                    <Swiper modules={[Navigation, Pagination, Scrollbar, A11y]} spaceBetween={0} slidesPerView={2} navigation pagination={{ clickable: true }} >
-                        <SwiperSlide className='w-1/4 max-w-[364px]' >
+        <div className='mt-20'>
+            <div className='w-80 mobile:w-full max-w-[380px] md:max-w-[740px] lg:w-full lg:max-w-[1100px] xl:max-w-[1230px] justify-between mx-auto flex items-center'>
+                <Image src={'assets/images/landing/Left-Arrow.svg'} height={20} width={20} onClick={handlePrev} />
+                <div className='w-[280px] mobile:w-80 md:w-[700px] lg:w-full lg:max-w-[1020] xl:max-w-[1230px] xl:w-full h-full overflow-hidden '>
+                    <div
+                        className='w-[840px] mobile:w-[960px] md:w-[1050px]  lg:w-full lg:max-w-[1100px] xl:max-w-[1230px] xl:w-full h-full flex'
+                        style={{
+                            transform: `translateX(${-currentIndex * (100 / numDivs)}%)`,
+                            transition: 'transform 0.3s ease-in-out',
+                        }}
+                    >
+                        <div className='w-[100%] h-full  flex justify-center'>
                             <ProductComp
                                 text={"Loop Lite"}
                                 img={"/assets/images/landing/LiteCard.png"}
+                                content={"Your brand's front-row showcase, in an affordable package."}
                             />
-                        </SwiperSlide>
-                        <SwiperSlide >
+                        </div>
+                        <div className='w-[100%] h-full  flex justify-center'>
                             <ProductComp
-                                text={"Loop Elevate"}
+                                text={"Loop Elevate "}
                                 img={"/assets/images/landing/ElevateCard.png"}
+                                content={"Where your brand captivates, in seamless sophistication."}
+
                             />
-                        </SwiperSlide>
-                        <SwiperSlide >
+                        </div>
+                        <div className='w-[100%] h-full  flex justify-center'>
                             <ProductComp
                                 text={"Loop Supreme"}
                                 img={"/assets/images/landing/SupremeCard.png"}
+                                content={"Where your brand commands attention, with unrivalled prestige."}
+
                             />
-                        </SwiperSlide>
-                    </Swiper>
+                        </div>
+                    </div>
                 </div>
-                <div className=' md:hidden'>
-                    <Swiper modules={[Navigation, Pagination, Scrollbar, A11y]} spaceBetween={50} slidesPerView={1} centeredSlides={true} navigation pagination={{ clickable: true }} >
-                        <SwiperSlide className='w-1/4 max-w-[364px]' >
-                            <ProductComp
-                                text={"Loop Lite"}
-                                img={"/assets/images/landing/LiteCard.png"}
-                            />
-                        </SwiperSlide>
-                        <SwiperSlide >
-                            <ProductComp
-                                text={"Loop Elevate"}
-                                img={"/assets/images/landing/ElevateCard.png"}
-                            />
-                        </SwiperSlide>
-                        <SwiperSlide >
-                            <ProductComp
-                                text={"Loop Supreme"}
-                                img={"/assets/images/landing/SupremeCard.png"}
-                            />
-                        </SwiperSlide>
-                    </Swiper>
-                </div>
-                
+                <Image src={'assets/images/landing/Right-Arrow.svg'} height={20} width={20} onClick={handleNext} />
             </div>
         </div>
-    )
+    );
 }
 
-export default Products
+export default Products;
--- components/landing/Swiper.jsx
+++ components/landing/Swiper.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import TestimonialComp from './TestimonialComp'
+import ProductComp from './ProductComp';
 
 function Swiper() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -49,13 +49,27 @@ function Swiper() {
             }}
           >
             <div className='w-[100%] h-full bg-slate-300 flex justify-center'>
-
+              <ProductComp
+                text={"Loop Lite"}
+                img={"/assets/images/landing/LiteCard.png"}
+                content={"Your brand's front-row showcase, in an affordable package."}
+              />
             </div>
             <div className='w-[100%] h-full bg-slate-500 flex justify-center'>
-              
+              <ProductComp
+                text={"Loop Elevate "}
+                img={"/assets/images/landing/ElevateCard.png"}
+                content={"Where your brand captivates, in seamless sophistication."}
+
+              />
             </div>
             <div className='w-[100%] h-full bg-slate-700 flex justify-center'>
+              <ProductComp
+                text={"Loop Supreme"}
+                img={"/assets/images/landing/SupremeCard.png"}
+                content={"Where your brand commands attention, with unrivalled prestige."}
 
+              />
             </div>
           </div>
         </div>
