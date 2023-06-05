7b40bbd4b9cd0456f1e4a2fbbf99e10dc0fcf9b8
amit <amit256375@gmail.com>
2023-06-05 18:50:24 +0530

    changes in footer

--- components/landing/Footer.jsx
+++ components/landing/Footer.jsx
@@ -1,105 +1,160 @@
-import Landing from '@/pages'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Landing from "@/pages";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='' id='contact'>
-        <div className='hidden md:block'>
-        <div className=' h-[262px] min-w-full bg-gradient-to-tr from-[#66D3E1] to-[#FDFF96] flex justify-around items-center'>
-            <div className='hidden lg:block '>
-                <Image src={"/assets/images/landing/footerloop.svg"} height={81} width={186} alt='loop logo' />
-
+    <div className="" id="contact">
+      <div className="hidden md:block">
+        <div className="bg-gradient-to-tr from-[#66D3E1] to-[#FDFF96]">
+          <div className="flex justify-around items-center h-[262px] min-w-full ">
+            <div className="hidden lg:block ">
+              <Image
+                src={"/assets/images/landing/footerloop.svg"}
+                height={81}
+                width={186}
+                alt="loop logo"
+              />
             </div>
-            <div className='lg:hidden '>
-                <Image src={"/assets/images/landing/footerloop.svg"} height={81} width={156} alt='loop logo' />
-
+            <div className="lg:hidden ">
+              <Image
+                src={"/assets/images/landing/footerloop.svg"}
+                height={81}
+                width={156}
+                alt="loop logo"
+              />
             </div>
 
-            <div className='flex gap-x-8'>
-                <div className='flex flex-col gap-y-7 '>
+            <div className="flex gap-x-8">
+              <div className="flex flex-col gap-y-7 ">
                 <div>
-               <p className='text-[14px] font-bold'>privacy policy</p>
+                  <p className="text-[14px] font-bold">privacy policy</p>
                 </div>
-                <div className='flex space-x-5  items-center justify-center'>
-                    <div>
-                    <a href="https://instagram.com/loop.card?igshid=NTc4MTIwNjQ2YQ==" target="_blank">
-                        <Image src={'/assets/images/landing/footerinsta.png'} height={32} width={32} alt='loop instagram'/>
-                   </a>
-                    </div>
-                    <div>
-                    <a href="#">
-                    <Image src={'/assets/images/landing/footerlink.png'} height={32} width={32} alt='loop linkedin'/>
+                <div className="flex space-x-5  items-center justify-center">
+                  <div>
+                    <a
+                      href="https://instagram.com/loop.card?igshid=NTc4MTIwNjQ2YQ=="
+                      target="_blank"
+                    >
+                      <Image
+                        src={"/assets/images/landing/footerinsta.png"}
+                        height={32}
+                        width={32}
+                        alt="loop instagram"
+                      />
                     </a>
-                    </div>
-
+                  </div>
+                  <div>
+                    <a href="#">
+                      <Image
+                        src={"/assets/images/landing/footerlink.png"}
+                        height={32}
+                        width={32}
+                        alt="loop linkedin"
+                      />
+                    </a>
+                  </div>
                 </div>
-
-
-                </div>
-                <div>
-                <p className='text-[14px] font-bold'>terms and conditions</p>
-
-                </div>
-
+              </div>
+              <div>
+                <p className="text-[14px] font-bold">terms and conditions</p>
+              </div>
             </div>
 
+            <div className="flex flex-col ">
+              <div className="flex flex-col gap-y-4 ">
+                <div className="text-[14px] font-bold">
+                  <p>contact us </p>
+                </div>
+                <div className=" text-[16px] leading-[154%]">
+                  <p> gm@alphamintlabs.com</p>
+                </div>
+              </div>
+
+              <div className="flex items-center  space-x-1 pt-7 ">
+                <div className="text-xs">
+                  <p>Made with love by</p>
+                </div>
+                <div>
+                  <Image
+                    src={"/assets/images/landing/amlLogo.png"}
+                    alt="aml Logo"
+                    height={23}
+                    width={63}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="md:hidden min-w-full bg-gradient-to-tr from-[#66D3E1] to-[#FDFF96] h-full">
+        <div className="flex flex-col justify-center items-center">
+          <div className="pt-9">
+            <Image
+              src={"/assets/images/landing/footerloop.svg"}
+              height={46}
+              width={106}
+              alt="loop logo"
+            />
+          </div>
+          <div className=" pt-10 font-bold">
+            <p>privacy policy</p>
+          </div>
+          <div className=" pt-[30px] font-bold">
+            <p>terms and conditions</p>
+          </div>
+          <div className=" pt-[30px] font-bold">
+            <p>contact us </p>
+          </div>
+          <div className=" pt-2">
+            <p> gm@alphamintlabs.com </p>
+          </div>
+
+          <div className="flex  pt-8 space-x-7">
             <div>
-                <div className='flex flex-col gap-y-7'>
-                    <div className='text-[14px] font-bold'>
-                        <p>contact us </p>
-                    </div>
-                    <div className=' text-[16px] leading-[154%]'>
-                        <p> gm@alphamintlabs.com</p>
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        </div>
-
-        <div className='md:hidden min-w-full bg-gradient-to-tr from-[#66D3E1] to-[#FDFF96] h-[365px]'>
-            <div className='flex flex-col justify-center items-center'>
-                <div className='pt-9'>
-               <Image src={"/assets/images/landing/footerloop.svg"} height={46} width={106} alt="loop logo" />
-
-                </div>
-                <div className=' pt-10 font-bold'>
-                <p>privacy policy</p>
-                </div>
-                <div className=' pt-[30px] font-bold'>
-                 <p>terms and conditions</p>
-                </div>
-                <div className=' pt-[30px] font-bold'>
-                 <p>contact us </p>
-                </div>
-                <div className=' pt-2'>
-                 <p> gm@alphamintlabs.com </p>
-                </div>
-
-               <div className='flex  pt-8 space-x-7'>
-                <div>
-                <a href="https://instagram.com/loop.card?igshid=NTc4MTIwNjQ2YQ==" target="_blank">
-    <Image src="/assets/images/landing/footerinsta.png" height={32} width={32} alt="loop instagram" />
-  </a>
-                </div>
-                <div>
-                <a href="#">
-                <Image src={'/assets/images/landing/footerlink.png'} height={32} width={32} alt='loop linkedin'/>
+              <a
+                href="https://instagram.com/loop.card?igshid=NTc4MTIwNjQ2YQ=="
+                target="_blank"
+              >
+                <Image
+                  src="/assets/images/landing/footerinsta.png"
+                  height={32}
+                  width={32}
+                  alt="loop instagram"
+                />
               </a>
-                </div>
-
-               </div>
-
             </div>
-
+            <div>
+              <a href="#">
+                <Image
+                  src={"/assets/images/landing/footerlink.png"}
+                  height={32}
+                  width={32}
+                  alt="loop linkedin"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="flex items-center justify-center pb-2 pt-5 space-x-1 ">
+            <div className="text-xs">
+              <p>Made with love by</p>
+            </div>
+            <div>
+              <Image
+                src={"/assets/images/landing/amlLogo.png"}
+                alt="aml Logo"
+                height={25}
+                width={60}
+              />
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
