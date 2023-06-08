6d7d0076e616e91d83854013b61ea6219b169c55
amit <amit256375@gmail.com>
2023-06-08 10:45:48 +0530

    updated components

--- components/CartComps/CardDescription.jsx
+++ components/CartComps/CardDescription.jsx
@@ -5,9 +5,6 @@ import React, { useContext, useState, useEffect } from "react";
 const CardDescription = ({
   cardTypeSelected,
   handleCardSelection,
-  totalAmount,
-  totalQuantity,
-  selectedTypeIndex,
   heading,
   price,
   desc,
