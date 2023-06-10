c5249f8eec2d9573cbc95bbf58097b2780185beb
alphamintlabs <rohit@alphamintlabs.com>
2023-06-11 01:55:19 +0530

    Modified logo size of rotary

--- components/landing/Partners.jsx
+++ components/landing/Partners.jsx
@@ -20,7 +20,7 @@ const Partners = () => {
                         <Image src={'/assets/images/landing/partnerLogos/AMLBW.png'} height={98} width={282} className="mx-8" alt='AML Logo'/>
                         <Image src={'/assets/images/landing/partnerLogos/BravasLogoBW.png'} height={98} width={261} className="mx-8" alt='Bravas Digital Logo'/>
                         <Image src={'/assets/images/landing/partnerLogos/CeomitraLogoBW.png'} height={98} width={271} className="mx-8" alt='Ceomitra Logo'/>
-                        <Image src={'/assets/images/landing/partnerLogos/RotaryLogoBW.png'} height={98} width={98} className="mx-8" alt="Rotary Logo"/>
+                        <Image src={'/assets/images/landing/partnerLogos/RotaryLogoBW.png'} height={98} width={405} className="mx-8" alt="Rotary Logo"/>
                         <Image src={'/assets/images/landing/partnerLogos/AntalLogoBW.png'} height={98} width={99} className="mx-8" alt="Antal Thane Logo"/>
                         <Image src={'/assets/images/landing/partnerLogos/BlackCabBW.png'} height={98} width={107} className="mx-8" alt="Black Cab Logo"/>
                         {/* <Image src={'/assets/images/landing/partnerLogos/IABW.png'} height={61} width={200} className="mx-8" alt="Ingenious Advisors Logo"/> */}
@@ -37,7 +37,7 @@ const Partners = () => {
                         <Image src={'/assets/images/landing/partnerLogos/AMLBW.png'} height={49} width={141} className="mx-4" alt='AML Logo'/>
                         <Image src={'/assets/images/landing/partnerLogos/BravasLogoBW.png'} height={49} width={131} className="mx-4" alt='Bravas Digital Logo'/>
                         <Image src={'/assets/images/landing/partnerLogos/CeomitraLogoBW.png'} height={49} width={136} className="mx-4" alt='Ceomitra Logo'/>
-                        <Image src={'/assets/images/landing/partnerLogos/RotaryLogoBW.png'} height={49} width={49} className="mx-4" alt="Rotary Logo"/>
+                        <Image src={'/assets/images/landing/partnerLogos/RotaryLogoBW.png'} height={49} width={203} className="mx-4" alt="Rotary Logo"/>
                         <Image src={'/assets/images/landing/partnerLogos/AntalLogoBW.png'} height={49} width={49} className="mx-4" alt="Antal Thane Logo"/>
                         <Image src={'/assets/images/landing/partnerLogos/BlackCabBW.png'} height={49} width={53} className="mx-4" alt="Black Cab Logo"/>
                         {/* <Image src={'/assets/images/landing/partnerLogos/IABW.png'} height={31} width={100} className="mx-4" alt="Ingenious Advisors Logo"/> */}
---
+++ pages/PrivacyPolicy.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+function PrivacyPolicy() {
+  return (
+    <div className='  max-w-[1212px] mx-3 xl:mx-auto '>
+        
+        <h2 className='pt-12 mb-12'>Privacy Policy</h2>
+
+        <div>
+       
+       <p>At <span className='font-bold'>loopcard.club,</span> we take your privacy seriously and are committed to protecting the personal information you provide to us. This Privacy Policy outlines how we collect, use, disclose, and protect your personal information in accordance with applicable privacy laws and search engine compliances. By accessing or using our website <span className='font-bold'>www.loopcard.club</span>, you agree to the terms of this Privacy Policy.
+       </p>
+       
+
+       <ul>
+              <li> 1. Information We Collect:
+                   <p>We may collect the following types of personal information:</p>
+                   <ul>
+                      <li>Contact Information: Name, email address, phone number, and any other information you provide when contacting us or submitting an inquiry.</li>
+                      <li>Use of Personal Information:
+                           We may use your personal information for the following purposes:
+                           To respond to your inquiries and provide customer support.
+                           To improve and personalise your experience on our website.
+                           To send you updates, newsletters, and promotional communications, if you have opted to receive them.
+                           To comply with legal obligations and enforce our Terms of Service.</li>
+                   </ul>
+                   
+
+
+              </li>
+       </ul>
+
+       <ul>
+        <li>
+          1
+        </li>
+
+        <li>
+          2
+        </li>
+       </ul>
+        
+        <p className='font-bold mt-12'>1. Information We Collect:</p>
+        <p>We may collect the following types of personal information:</p>
+           Contact Information: Name, email address, phone number, and any other information you provide when contacting us or submitting an inquiry.
+
+        Use of Personal Information:
+        We may use your personal information for the following purposes:
+        To respond to your inquiries and provide customer support.
+        To improve and personalise your experience on our website.
+        To send you updates, newsletters, and promotional communications, if you have opted to receive them.
+        To comply with legal obligations and enforce our Terms of Service.
+
+Disclosure of Personal Information:
+We may disclose your personal information to third parties in the following circumstances:
+With your consent or as necessary to fulfil the purpose for which the information was provided.
+To trusted third-party service providers who assist us in operating our website, conducting business, or providing services to you.
+To comply with applicable laws, regulations, or legal processes.
+
+Data Security
+We take appropriate security measures to protect your personal information from unauthorised access, disclosure, alteration, or destruction. However, please note that no method of transmission over the Internet or electronic storage is 100% secure.
+
+Cookies and Tracking Technologies
+Our website may use cookies and other tracking technologies to enhance your browsing experience and collect non-personal information. You have the option to disable cookies through your browser settings.
+
+Third-Party Links
+Our website may contain links to third-party websites. We are not responsible for the privacy practices or content of those websites. We encourage you to review the privacy policies of any third-party websites you visit.
+
+Children's Privacy
+Our website is not intended for individuals under the age of 13. We do not knowingly collect personal information from children. If we become aware that we have inadvertently collected personal information from a child under the age of 13, we will take appropriate steps to delete it.
+
+Updates to the Privacy Policy
+We may update this Privacy Policy from time to time. We will notify you of any material changes by posting the updated Privacy Policy on our website. Your continued use of our website following the posting of changes constitutes your acceptance of such changes.
+
+Contact Us
+If you have any questions, concerns, or requests regarding this Privacy Policy or the handling of your personal information, please contact us at gm@alphamintlabs.com.
+
+This Privacy Policy aims to comply with applicable privacy laws and search engine compliance. It covers essential aspects such as information collection, use, disclosure, data security, cookies, and provides contact information for inquiries or concerns. Remember to review and customise the privacy policy to fit your specific website and legal requirements.
+
+        </div>
+
+
+
+    </div>
+
+  )
+}
+
+export default PrivacyPolicy
