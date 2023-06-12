a32a4d789081ab429bd66a77605d5cb63e8a1db6
amit <amit256375@gmail.com>
2023-06-12 13:57:17 +0530

    edited slug

--- pages/details/[slug].jsx
+++ pages/details/[slug].jsx
@@ -1,80 +1,326 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import fetch from 'isomorphic-unfetch';
-import { saveAs } from 'file-saver'; // Import the file-saver library
-import ProfileImg from '@/components/ProfileImg';
-import Bio from '@/components/Bio';
-import Social from '@/components/Social';
-import Image from 'next/image';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import fetch from "isomorphic-unfetch";
+import { saveAs } from "file-saver"; // Import the file-saver library
+import ProfileImg from "@/components/ProfileImg";
+import Bio from "@/components/Bio";
+import Social from "@/components/Social";
+import Image from "next/image";
+import axios from "axios";
+import Socialro from "@/components/rotary/Socialro";
+import Bioro from "@/components/rotary/Bioro";
+import Profilero from "@/components/rotary/Profilero";
+import RotaryLogo from "@/components/rotary/RotaryLogo";
+import Template from "@/components/Template";
+import themes from "@/components/Themes";
 //import Navbar from '@/components/Navbar';
 
 export default function ContactDetails() {
   const router = useRouter();
   const [contact, setContact] = useState(null);
+  const [id, setId] = useState(null);
 
   useEffect(() => {
     const { slug } = router.query;
-    async function fetchContact() {
-      const res = await fetch(`/api/contact?cardUuid=${slug}`);
-      const data = await res.json();
-      if (data.contact) {
-        setContact(data.contact);
+    setId(slug);
 
-        console.clear();
+    // async function fetchContact() {
+    //   const res = await fetch(`/api/contact?cardUuid=${slug}`);
+    //   const data = await res.json();
+    //   if (data.contact) {
+    //     setContact(data.contact);
 
-        const cardUuid = data.contact.cardUuid;
-        const userEmail = data.contact.email;
+    //     console.clear();
 
-        const sendDataToAPI = { cardUuid, userEmail }
+    //     const cardUuid = data.contact.cardUuid;
+    //     const userEmail = data.contact.email;
 
-        const settingRemainder = await axios.post('http://localhost:3000/api/setRemainder', sendDataToAPI)
-      }
-    }
+    //     const sendDataToAPI = { cardUuid, userEmail };
 
-    fetchContact();
+    //     const settingRemainder = await axios.post(
+    //       "http://localhost:3000/api/setRemainder",
+    //       sendDataToAPI
+    //     );
+    //   }
+    // }
+
+    //fetchContact();
   }, [router.query]);
+
+  //   const downloadVCard = () => {
+  //     // Create a vCard string from the contact data
+  //     const vCard = `BEGIN:VCARD
+  // FN:${contact.firstName} ${contact.lastName}
+  // TEL;TYPE=CELL:${contact.mobileNumber}
+  // EMAIL;:${contact.email}
+  // END:VCARD`;
+
+  //     // Convert the vCard string to a Blob object
+  //     const blob = new Blob([vCard], { type: "text/vcard;charset=utf-8" });
+
+  //     // Save the Blob as a file using the file-saver library
+  //     saveAs(blob, `${contact.firstName} ${contact.lastName}.vcf`);
+  //   };
 
   const downloadVCard = () => {
     // Create a vCard string from the contact data
     const vCard = `BEGIN:VCARD
-FN:${contact.firstName} ${contact.lastName}
-TEL;TYPE=CELL:${contact.mobileNumber}
-EMAIL;:${contact.email}
+FN:Sukumaran Nair
+TEL;TYPE=CELL:+919819697361
+EMAIL;: siddharth@alphamintlabs.com
+URL:rchiranandani.rotaryindia.org
 END:VCARD`;
 
     // Convert the vCard string to a Blob object
-    const blob = new Blob([vCard], { type: 'text/vcard;charset=utf-8' });
+    const blob = new Blob([vCard], { type: "text/vcard;charset=utf-8" });
 
     // Save the Blob as a file using the file-saver library
-    saveAs(blob, `${contact.firstName} ${contact.lastName}.vcf`);
+    saveAs(blob, "hello.vcf");
   };
-  // const name=contact.firstName +" "+ contact.lastName;
   return (
-    <div className='bg-black '>
+    <div className=" ">
+      {/* ayush bansal */}
+      {id == "d5d7d6f3-2583-4c9b-af23-6a712e3b4d4d" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Aayush"}
+            lname={"Bansal"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Co-Founder"}
+            company={"Black Cab"}
+            website={"https://blackcab.co.in/"}
+            mobile={"9820447476"}
+            gmail={"aayush@blackcab.co.in"}
+            instaLink={`https://www.instagram.com/aayushbansal26/`}
+            watspLink={`https://api.whatsapp.com/send?phone=919820447476`}
+          />
+        </div>
+      )}
 
-      <div className=" bg-gradient-to-b from-[#D2FFEC] via-[#F16869] to-[#FF932F] w-full mx-auto sm:w-[640px] pt-[146px]">
-        {contact ? (
-          <div className='relative px-6 bg-gradient-to-b from-[#FFFFFF] to-[#B0B0B0] mx-auto h-[612px] w-[351px] rounded-[20px]'>
+      {/*  2*/}
+      {id == "6b18db9c-0c96-45c5-ba77-04dc3293dc7c" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Siddhartha"}
+            lname={"Singh"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Co-Founder"}
+            company={"Black Cab"}
+            website={"https://blackcab.co.in/"}
+            mobile={"9004922756"}
+            gmail={"siddhartha@blackcab.co.in"}
+            instaLink={"https://www.instagram.com/siddharthasingh05/"}
+            watspLink={`https://api.whatsapp.com/send?phone=919004922756`}
+          />
+        </div>
+      )}
 
-
-
-            <div className='-top-16 inset-0 mx-auto absolute h-[100px] w-[100px] '>      <ProfileImg /></div>
-
-            <div className='flex justify-center pt-[52px] font-semibold text-[20px]'>{contact.firstName} {contact.lastName}</div>
-            <div className='flex justify-center mt-[11px] font-semibold text-[16px]'>Design Lead</div>
-            <div><Bio phone={contact.mobileNumber} /></div>
-            <div><Social /></div>
-            <div className=''><button className='  bg-black font-bold text-[20px] text-white h-[50px] w-[304px] rounded-[14px] mt-[31px]' onClick={downloadVCard}>SAVE CONTACT</button>  </div>
-
-
-          </div>
-        ) : (
-          <p>Loading...</p>
-        )}
-        <div className='text-[10px] flex justify-center mt-8 pb-[29px]'>made with love by <Image className='ml-3' alt='loop' src={'/loop.png'} width={27} height={12} /></div>
-      </div>
+      {/* 3 */}
+      {id == "a82d7d0e-7e32-4fe5-a4b2-cfe79f0c4c4f" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Aayushman"}
+            lname={"Sinha"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Co-Founder"}
+            company={"Black Cab"}
+            website={"https://blackcab.co.in/"}
+            mobile={"9833556650"}
+            gmail={"aayushman@blackcab.co.in"}
+            instaLink={`https://www.instagram.com/aayushmansinha/`}
+            watspLink={`https://api.whatsapp.com/send?phone=919833556650`}
+          />
+        </div>
+      )}
+      {/* 4 */}
+      {id == "7a932f36-70b4-4ad5-bd6a-91139b898874" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Rishikesh"}
+            lname={"Bhanushali"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Co-Founder"}
+            company={"Black Cab"}
+            website={"https://blackcab.co.in/"}
+            mobile={"9920223594"}
+            gmail={"rishikesh@blackcab.co.in "}
+            instaLink={`https://www.instagram.com/rishi_235/`}
+            watspLink={`https://api.whatsapp.com/send?phone=919920223594`}
+          />
+        </div>
+      )}
+      {/* 5 */}
+      {id == "0a7f5a3d-6b2d-4740-bd34-2285792344b6" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Alok"}
+            lname={"Verma"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Co-Founder"}
+            company={"Secret Sauce Studios- A Black Cab Network Agency "}
+            website={"https://secretsaucestudios.in/"}
+            mobile={"9930981280"}
+            gmail={"alok@secretsaucestudios.in "}
+            instaLink={`https://www.instagram.com/allaboutthatpalate/`}
+            watspLink={`https://api.whatsapp.com/send?phone=919930981280`}
+          />
+        </div>
+      )}
+      {/* 6 */}
+      {id == "f50f7844-7b2e-4459-82c3-2f82e32d1026" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Nishant"}
+            lname={"Joshi"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Co-Founder"}
+            company={"Secret Sauce Studios- A Black Cab Network Agency "}
+            website={"https://secretsaucestudios.in/"}
+            mobile={"9819566568"}
+            gmail={"nishant@secretsaucestudios.in "}
+            instLink={`https://www.instagram.com/joshinishant91/`}
+            watspLink={`https://api.whatsapp.com/send?phone=919819566568`}
+          />
+        </div>
+      )}
+      {/* 7 */}
+      {id == "9b05175e-eeb3-4d71-8a36-1e2b11fe5d75" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Ujjwal"}
+            lname={"Sharma"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Co-Founder"}
+            company={"Secret Sauce Studios- A Black Cab Network Agency "}
+            website={"https://radaragency.io/"}
+            mobile={"9999234245"}
+            gmail={"ujjwal@radaragency.io "}
+            instaLink={"https://www.instagram.com/ujjwalsharmma/"}
+            watspLink={`https://api.whatsapp.com/send?phone=919999234245`}
+          />
+        </div>
+      )}
+      {/* 8 */}
+      {id == "c45748fe-20e5-4cfe-a8c9-4ed0f1061f5a" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Sandesh"}
+            lname={"Jha"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Co-Founder"}
+            company={"Black Cab"}
+            website={"https://blackcab.co.in/"}
+            mobile={"8693882326"}
+            gmail={"sandesh@m-3.in"}
+            instaLink={`https://www.instagram.com/theperpetualdaydreamer/`}
+            watspLink={`https://api.whatsapp.com/send?phone=918693882326`}
+          />
+        </div>
+      )}
+      {/* 9 */}
+      {id == "3beaf24d-7c8f-47a5-8796-9f8d47706712" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Sidhantha"}
+            lname={"Jain"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Co-Founder"}
+            company={"Black Cab"}
+            website={"https://blackcab.co.in/"}
+            mobile={"8700118994"}
+            gmail={"sid@m-3.in"}
+            instaLink={`https://www.instagram.com/theperpetualdaydreamer/`}
+            watspLink={`https://api.whatsapp.com/send?phone=918700118994`}
+          />
+        </div>
+      )}
+      {/* 10 */}
+      {id == "64350fc2-7f97-4b54-82e1-82b3f2cfc7ff" && (
+        <div className="min-w-[375px]  flex ml-4">
+          <Template
+            gradient1={themes[0].gradient1}
+            gradient2={themes[0].gradient2}
+            text1={themes[0].text1}
+            text2={themes[0].text2}
+            text3={themes[0].text3}
+            btn={themes[0].btn}
+            loop={themes[0].loop}
+            fname={"Khushi"}
+            lname={"Gadhia"}
+            bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "}
+            designation={"Executive Assistant "}
+            company={"Black Cab"}
+            website={"https://blackcab.co.in/"}
+            mobile={"9167837466"}
+            gmail={"khushi@blackcab.co.in"}
+            instaLink={`https://www.instagram.com/khushigadhiaa/`}
+            watspLink={`https://api.whatsapp.com/send?phone=919167837466`}
+          />
+        </div>
+      )}
     </div>
   );
 }
-
