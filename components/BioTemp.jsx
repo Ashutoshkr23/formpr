9b533ef44ac07274b7827453ba14c996e37a23eb
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-06-14 11:48:05 +0530

    biotemp

--- components/BioTemp.jsx
+++ components/BioTemp.jsx
@@ -19,47 +19,42 @@ const BioTemp = ({ bio, border, company, website, mobile, text1, text2 }) => {
               height={22}
             />
 
-          <p className="text-[8px] ml-[2px] tracking-[-0.5px] text-[#7D9695]  ">
-            {company ? company : "Alphamintlabs"}
-          </p>
-        </div>
-        <div className="flex ml-[50px] ">
-          <Image
-            src={"/assets/images/display/phone.png"}
-            alt="phone"
-            width={22}
-            height={22}
-          />
-          <p className="text-[10px] ml-[1px] tracking-[-0.5px]">
-            <input
-              className={`bg-transparent w-20 ${text2} text-[#7D9695] `}
+            <p className="text-[10px] ml-[2px] tracking-[-0.5px] text-[#7D9695]  ">
+              {company}
+              {/* <input
+              className={`bg-transparent ${text2} text-[#7D9695] w-[120px]  `}
               type="text"
-              placeholder="+919833XXXXXX"
-              value={mobile}
+              placeholder="company Name"
+              value={company}
               readOnly={true}
               //   onChange={handleChange}
-            />
-          </p>
-        </div>
-        <div className="flex pt-1">
-          <Image
-            src={"/assets/images/display/website.png"}
-            alt="website"
-            width={22}
-            height={22}
-          />
-          <Link href={`${website}`}>
-            <p className="text-[10px] ml-1.5 tracking-[-0.5px]">
-              <input
-                className={`bg-transparent ${text2} text-[#7D9695] cursor-pointer w-[200px] `}
-                type="text"
-                placeholder=" www.alphamintlabs.com"
-                value={website}
-                readOnly={true}
-                //   onChange={handleChange}
-              />
+            /> */}
             </p>
-          </Link>
+          </div>
+          <div className="flex items-center space-x-2 ml-[50px] ">
+            <Image
+              src={"/assets/images/display/phone.png"}
+              alt="phone"
+              width={22}
+              height={22}
+            />
+            <p className="text-[10px] ml-[1px] tracking-[-0.5px] text-[#7D9695]">
+              {mobile}
+            </p>
+          </div>
+          <div className="flex items-center pt-1">
+            <Image
+              src={"/assets/images/display/website.png"}
+              alt="website"
+              width={22}
+              height={22}
+            />
+            <Link href={`${website}`}>
+              <p className="text-[10px] ml-1.5 tracking-[-0.5px] text-[#7D9695] cursor-pointer  ">
+                {website}
+              </p>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
