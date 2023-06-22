944e2d67e1d4744e2eb8a2f5237ad73ee123872f
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-06-22 13:37:47 +0530

    Profile Completed section responsive

--- components/TemplateComp/FormComp/ProfileCompleted.jsx
+++ components/TemplateComp/FormComp/ProfileCompleted.jsx
@@ -4,13 +4,15 @@ import Link from 'next/link'
 
 function ProfileCompleted() {
   return (
-      <div className='fixed top-0 right-0 z-[50] w-screen h-screen bg-white flex items-center justify-center  bg-opacity-50 backdrop-blur-md   '>
-          <div className=' h-[560px]  w-[746px] flex flex-col items-center py-10 bg-gradient-to-r from-[#67D4E1] to-[#8DF6B8] rounded-[10px] '>
-            <Image src="/assets/images/ProfileCompleted.png" height={257} width={359} className='pl-20' alt="profile-completed icon"/>
-            <h1 className='text-[40px] font-bold  mt-10 '>Profile Creation Completed</h1>
+      <div className='fixed top-0 right-0 z-[50] w-screen h-screen bg-white flex items-center justify-center  bg-opacity-50 backdrop-blur-md p-4 sm:p-10   '>
+          <div className='h-[500px] w-full sm:h-[560px]  sm:w-[746px] flex flex-col items-center py-10 bg-gradient-to-r from-[#67D4E1] to-[#8DF6B8] rounded-[10px] '>
+            <div className='relative h-[195px] sm:h-[257px] w-[212px] sm:w-[359px]'>
+            <Image src="/assets/images/ProfileCompleted.png" fill={true} className='sm:ml-10' alt="profile-completed icon"/>
+        </div>
+            <h1 className='text-xl sm:text-[40px] font-bold  mt-10 '>Profile Creation Completed</h1>
             <div className='pt-20'>
                   <Link href="/manageCards">
-                      <button className="h-10 w-[388px] rounded-[10px] bg-black text-white">
+                      <button className="h-10 px-2 w-full sm:w-[388px] rounded-[10px] bg-black text-white text-xs sm:text-base">
                           Go to Manage Cards and view your profile
                       </button>
                   </Link>
