04b107492a3be18d93ca4c70b45cfd9cb90a4a94
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-06-22 12:59:15 +0530

    More Alt tags added

--- components/TemplateComp/FormComp/ProfileCompleted.jsx
+++ components/TemplateComp/FormComp/ProfileCompleted.jsx
@@ -6,7 +6,7 @@ function ProfileCompleted() {
   return (
       <div className='fixed top-0 right-0 z-[50] w-screen h-screen bg-white flex items-center justify-center  bg-opacity-50 backdrop-blur-md   '>
           <div className=' h-[560px]  w-[746px] flex flex-col items-center py-10 bg-gradient-to-r from-[#67D4E1] to-[#8DF6B8] rounded-[10px] '>
-            <Image src="/assets/images/ProfileCompleted.png" height={257} width={359} className='pl-20 '/>
+            <Image src="/assets/images/ProfileCompleted.png" height={257} width={359} className='pl-20' alt="profile-completed icon"/>
             <h1 className='text-[40px] font-bold  mt-10 '>Profile Creation Completed</h1>
             <div className='pt-20'>
                   <Link href="/manageCards">
