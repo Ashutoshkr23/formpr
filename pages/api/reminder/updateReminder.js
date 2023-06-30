cccd9bb18fa8a276bd1c0ac2d8b5138edd5a4e2f
Priyal Raj <priyalraj99@gmail.com>
2023-06-30 18:05:09 +0530

    Reminder Logic Completed
    Merged With 2c1b52b, Chances Of Deployment Fail.

--- pages/api/reminder/updateReminder.js
+++ pages/api/reminder/updateReminder.js
@@ -50,44 +50,96 @@ export default async function handler(req, res) {
                 // Converting The Time Difference To Milliseconds
                 const milliseconds = Math.abs(difference);
 
-                // ! console.log(updatedReminder);
-                const message = {
-                    to: updatedReminder.userEmail,
-                    from: process.env.SENDGRID_EMAIL_ID,
-                    subject: 'Reminder Updated',
-                    text: 'Your reminder has been updated successfully.',
-                    html: `You have a meeting with ${userName} On ${formattedDateTime}. Their phone number is ${userContactNumber}. ${userCustomMessage}`
-                };
+                // ! If Time Difference Is Greater That 30 Days
+                // ^ Then It Will Run An Interval At Every Hour
+                // & And If Time Difference Is Less Than 1 Hour
+                // * It Will Send The Mail To User After A Timeout Of 1 Minute
+                // ~ But If User Disabled Their Reminder After timeDifference < 3600000
+                // ! It Will Not Send The Reminder
+                if (milliseconds > 2592000000) {
+                    setInterval(async () => {
+                        const findReminder = await setRemainderModel.find({ sendReminder: true, expired: false });
 
-                if (milliseconds > 0) {
-                    setTimeout(async () => {
-                        const checkReminder = await setRemainderModel.findOne({ _id: itemID }).select('sendReminder');
+                        if (findReminder.length === 0) {
 
-                        if (checkReminder === null) {
-                            return false;
-                        }
-                        if (checkReminder.sendReminder === false) {
-                            return;
-                        }
+                            console.log("No Reminders Found!");
 
-                        // Code To Send The Reminder To The User
-                        try {
-                            const expireTheReminder = await setRemainderModel.findOneAndUpdate({ _id: itemID }, { expired: true }, { new: true });
-                            await sgMail.send(message);
-                            console.log('Notification email sent successfully');
-                        } catch (error) {
-                            console.error('Error sending notification email:', error);
+                        } else if (findReminder.length >= 1) {
+
+                            console.log(findReminder.length + " Reminders Found.");
+
+                            for (let i = 0; i < findReminder.length; i++) {
+
+                                const timestamp1 = new Date(findReminder[i].customDate);
+                                const timestamp2 = new Date();
+                                const timeDifference = Math.abs(timestamp2 - timestamp1);
+
+                                console.log(timeDifference);
+
+                                if (timeDifference < 3600000) {
+                                    console.log("Called");
+                                    setTimeout(async () => {
+                                        if (findReminder.sendReminder === true && findReminder.expired === false) {
+                                            const findReminderID = findReminder._id;
+                                            const expireTheReminder = await setRemainderModel.findOneAndUpdate({ _id: findReminderID }, { expired: true }, { new: true });
+                                            const message = {
+                                                to: findReminder.userEmail,
+                                                from: process.env.SENDGRID_EMAIL_ID,
+                                                subject: 'Reminder Updated',
+                                                text: 'Your reminder has been updated successfully.',
+                                                html: `You have a meeting with ${findReminder[i].name} On ${findReminder[i].customDate}. Their phone number is ${findReminder[i].contactNumber}. ${findReminder[i].customMessage}`
+                                            };
+                                            await sgMail.send(message);
+                                            console.log('Notification email sent successfully');
+                                        } else {
+                                            console.log("Mail Not Send, Because Reminder Disabled!");
+                                        }
+                                    }, 60000);
+                                }
+                            }
                         }
-                    }, milliseconds);
+                    }, 3600000);
                 } else {
-                    // console.log('Selected date is in the past. No notification will be sent.');
+
+                    const message = {
+                        to: updatedReminder.userEmail,
+                        from: process.env.SENDGRID_EMAIL_ID,
+                        subject: 'Reminder Updated',
+                        text: 'Your reminder has been updated successfully.',
+                        html: `You have a meeting with ${userName} On ${formattedDateTime}. Their phone number is ${userContactNumber}. ${userCustomMessage}`
+                    };
+
+                    if (milliseconds > 0) {
+                        setTimeout(async () => {
+                            const checkReminder = await setRemainderModel.findOne({ _id: itemID }).select('sendReminder');
+
+                            if (checkReminder === null) {
+                                return false;
+                            }
+                            if (checkReminder.sendReminder === false) {
+                                return;
+                            }
+
+                            // Code To Send The Reminder To The User
+                            try {
+                                const expireTheReminder = await setRemainderModel.findOneAndUpdate({ _id: itemID }, { expired: true }, { new: true });
+                                await sgMail.send(message);
+                                console.log('Notification email sent successfully');
+                            } catch (error) {
+                                console.error('Error sending notification email:', error);
+                            }
+                        }, milliseconds);
+                    } else {
+                        // console.log('Selected date is in the past. No notification will be sent.');
+                    }
+                    return res.json({
+                        success: true,
+                        // updatedReminder: updatedReminder
+                    });
                 }
-                return res.json({
-                    success: true,
-                    // updatedReminder: updatedReminder
-                });
             }
-        } catch (error) {
+        }
+        catch (error) {
             logger.fatal(`Error on updateRemider ,Location:updateRemider ,error:${error}`);
             res.status(400).json({
                 error: true,
