14aa54849424298188657fbe9e5abf87550fe2e4
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-07-04 10:48:39 +0530

    Swiper Demo Build for supreme

--- pages/demo.jsx
+++ pages/demo.jsx
@@ -1,29 +1,11 @@
+import SupremeSwiper from '@/components/CartComps/SupremeSwiper'
 import React from 'react'
 
 
 function demo() {
   return (
     <div>
-      <div className='flex '>
-        <div className='h-10 w-full bg-red-300'></div>
-        <button className='h-10 w-40 bg-black text-white '>drag</button>
-      </div>
-          <div className='flex '>
-              <div className='h-10 w-full bg-red-400'></div>
-              <button className='h-10 w-40 bg-black text-white '>drag</button>
-          </div>
-          <div className='flex '>
-              <div className='h-10 w-full bg-red-500'></div>
-              <button className='h-10 w-40 bg-black text-white '>drag</button>
-          </div>
-          <div className='flex '>
-              <div className='h-10 w-full bg-red-600'></div>
-              <button className='h-10 w-40 bg-black text-white '>drag</button>
-          </div>
-          <div className='flex '>
-              <div className='h-10 w-full bg-red-700'></div>
-              <button className='h-10 w-40 bg-black text-white '>drag</button>
-          </div>
+      <SupremeSwiper/>
     </div>
   )
 }
